9268c4ea2b7e0041949522ad55bcfd4b48beb157
nour al maleh <nourmale7@gmail.com>
2022-10-23 13:55:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names,event) {
+  const cards=[ ]
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+writeCards(['Guadalupe', 'Ollie', 'Aki'], "surprise");
+
+
+function countDown(){
+  let i = 10;
+  while ( i > -1 ) {
+    console.log(i--);
+  }
+}
+countDown(10)
